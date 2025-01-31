90f6ea5c09e1ed9c759f486a466f4f16ed580543
irfanshka97 <irfanshka97@gmail.com>
2025-01-31 18:36:31 +0530

    ..

--- client/src/component/header/Header.jsx
+++ client/src/component/header/Header.jsx
@@ -1,18 +1,22 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
+import { Link } from "react-router-dom";
 
 export function Header() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home">Home Page</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link href="#home">Login</Nav.Link>
-            <Nav.Link href="#link">Signup</Nav.Link>
+            <Nav.Link as={Link} path="/login" className="nav-link">
+              Login
+            </Nav.Link>
+            <Nav.Link as={Link} path="/register" className="nav-link">
+              Signup
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
